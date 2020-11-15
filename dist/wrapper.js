80f8e15ce02a6634c0dc8605d99758536910109e
Illya Petrov <ilya.muromec@gmail.com>
2020-11-15 20:07:57 +0100

    wrap it

--- dist/wrapper.js
+++ dist/wrapper.js
@@ -1,6 +1,11 @@
 const getdstu7564 = require('./dstu7564.js');
 
-function copyTo(c, buffer) {
+let _wait = getdstu7564().then((_ret)=> {
+  c = _ret;
+  _wait = true;
+});
+
+function copyTo(buffer) {
   const ptr = c._malloc(buffer.length);
   for(let idx = 0; idx < buffer.length; idx++) {
     c.HEAPU8[ptr + idx] = buffer[idx];
@@ -8,12 +13,16 @@ function copyTo(c, buffer) {
   return ptr;
 }
 
-function copyFrom(c, ptr, len) {
+function copyFrom(ptr, len) {
   return Buffer.from(c.HEAPU8.slice(ptr, ptr + len));
 }
 
-function computeHash(c, buffer) {
-  const cbuffer = copyTo(c, buffer);
+function ready() {
+  return _wait
+}
+
+function computeHash(buffer) {
+  const cbuffer = copyTo(buffer);
   const chash = c._malloc(32);
 
   const ctx = c._dstu7564_alloc(0);
@@ -22,18 +31,9 @@ function computeHash(c, buffer) {
   c._dstu7564_final(ctx, chash);
   c._free(cbuffer);
 
-  const ret = copyFrom(c, chash, 32);
+  const ret = copyFrom(chash, 32);
   c._free(chash);
   return ret;
 }
 
-async function main() {
-  const buffer = Buffer.from('000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F202122232425262728292A2B2C2D2E2F303132333435363738393A3B3C3D3E3F', 'hex');
-
-  const c = await getdstu7564();
-  const hash = computeHash(c, buffer);
-
-  console.log('buffer', hash);
-}
-
-main();
+module.exports = {ready, computeHash};
--- test.js
+++ test.js
@@ -1,39 +1,15 @@
-const getdstu7564 = require('./dist/dstu7564.js');
-
-function copyTo(c, buffer) {
-  const ptr = c._malloc(buffer.length);
-  for(let idx = 0; idx < buffer.length; idx++) {
-    c.HEAPU8[ptr + idx] = buffer[idx];
-  }
-  return ptr;
-}
-
-function copyFrom(c, ptr, len) {
-  return Buffer.from(c.HEAPU8.slice(ptr, ptr + len));
-}
-
-function computeHash(c, buffer) {
-  const cbuffer = copyTo(c, buffer);
-  const chash = c._malloc(32);
-
-  const ctx = c._dstu7564_alloc(0);
-  c._dstu7564_init(ctx, 32);
-  c._dstu7564_update(ctx, cbuffer, buffer.length);
-  c._dstu7564_final(ctx, chash);
-  c._free(cbuffer);
-
-  const ret = copyFrom(c, chash, 32);
-  c._free(chash);
-  return ret;
-}
+const dstu7564 = require('./dist/wrapper.js');
 
 async function main() {
+  await dstu7564.ready();
   const buffer = Buffer.from('000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F202122232425262728292A2B2C2D2E2F303132333435363738393A3B3C3D3E3F', 'hex');
+  const expected = Buffer.from('08F4EE6F1BE6903B324C4E27990CB24EF69DD58DBE84813EE0A52F6631239875', 'hex');
 
-  const c = await getdstu7564();
-  const hash = computeHash(c, buffer);
+  const hash = dstu7564.computeHash(buffer);
 
   console.log('buffer', hash);
+  console.log('expected', expected);
+  console.log('OK?', hash.equals(expected));
 }
 
 main();
